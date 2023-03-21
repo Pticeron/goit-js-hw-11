f4d39d2dcf83f3fc0b95f88718e4ce9c09b11b11
Inna Zorina <innazorina0101@gmail.com>
2023-03-21 21:20:09 +0200

    commit to main

--- src/index.js
+++ src/index.js
@@ -7,3 +7,26 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 //   .then(res => {
 //     console.log(res.data);
 //   });
+
+function renderCards(cards) {
+    const markup = cards.markup(() => {
+        return `
+        <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`
+    })
+}
