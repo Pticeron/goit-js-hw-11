826533adf1c664bf65076d69358712e91d3e220b
Inna Zorina <innazorina0101@gmail.com>
2023-03-21 07:25:14 +0200

    links libraries

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// axios.get('/users')
+//   .then(res => {
+//     console.log(res.data);
+//   });
